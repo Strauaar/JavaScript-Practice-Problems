36b00f373920e5205d93c172a571c62f6ae6348d
Aaron <astrauli@gmail.com>
2017-12-23 19:24:11 -0800

    add linked lists

--- arrays.js
+++ arrays.js
@@ -34,3 +34,52 @@ const no_dups = (string) => {
 };
 
 console.log(no_dups("aasdd"));
+
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+//using sentinel nodes
+class LinkedList {
+  constructor() {
+    this.head = new Node();
+    this.tail = new Node();
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  push(key, value) {
+    let new_node = new Node(key,value);
+    this.tail.prev.next = new_node;
+    this.tail.prev = new_node;
+  }
+
+  delete(key,value) {
+    let node = this.head.next;
+    while(node !== null) {
+      if(node.key === key) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+      }
+    }
+  }
+
+  to_s() {
+    let node = this.head.next;
+    while(node !== null) {
+      console.log("key", node.key);
+      console.log("value", node.value);
+      node = node.next;
+    }
+  }
+}
+
+let list = new LinkedList;
+list.push(1, 1);
+list.push(2, 4);
+list.to_s();
